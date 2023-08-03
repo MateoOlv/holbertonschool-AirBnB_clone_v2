6b1f1dfffe38f4f274b537abc02c9b8b456937eb
MateoOlv <6304@holbertonstudents.com>
2023-08-03 13:50:44 -0700

    Back to task 5 version

--- models/city.py
+++ models/city.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    state_id = column(String(128), nulleable=False)
-    name = Column(string(60), ForeignKey('states.id'), nulleable=False)
+    state_id = ""
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
-    """
-    State
-    """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    
-    if models.storage_type == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
-    elif models.storage_type == 'file':
-        @property
-        def cities(self):
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+
+class State(BaseModel):
+    """ State class """
+    name = ""
