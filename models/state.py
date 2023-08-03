69d72ea0a63450c7fafeab3e3929f2fc61eda7e1
MateoOlv <6304@holbertonstudents.com>
2023-08-03 16:24:22 -0300

    Task 6 - State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
-
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    
+    if models.storage_type == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    elif models.storage_type == 'file':
+        @property
+        def cities(self):
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
