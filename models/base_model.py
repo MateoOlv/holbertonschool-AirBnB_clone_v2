645dad4a83d5d3521e87a268999543dbef3c33ce
MateoOlv <124683293+MateoOlv@users.noreply.github.com>
2023-08-03 23:33:44 -0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,6 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow(), onupdate=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
