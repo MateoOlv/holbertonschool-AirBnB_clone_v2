a96f702774314ddba68d9ac24bcbec6ed202e809
MateoOlv <6304@holbertonstudents.com>
2023-08-03 20:51:40 -0700

    Task 6 - Import order

--- models/state.py
+++ models/state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+from os import getenv
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
 
 class State(BaseModel, Base):
     """
