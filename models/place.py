affce8872ef9013d995795c001c6a4b312e14446
MateoOlv <6304@holbertonstudents.com>
2023-08-05 14:28:00 -0700

    Revert "task 10"
    This reverts commit 3e498544f4cc399505f8a18633b1d5210ea809d7.

--- models/place.py
+++ models/place.py
@@ -2,16 +2,17 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy import Column, String, ForeignKey, Float, Integer, Table
+from sqlalchemy import Column, String, ForeignKey, Float, Integer
 from sqlalchemy.orm import relationship
 
-place_amenity = Table(
-    "place_amenity",
-    Base.metadata,
-    Column("place_id", String(60), ForeignKey("places.id"), nullable=False),
-    Column(
-        "amenity_id", String(60), ForeignKey("amenities.id"), nullable=False))
 
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    metadata = Base.metadata
+    place_amenity = Table("place_amenity", metadata,
+                          Column("place_id", String(60), ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60), ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
