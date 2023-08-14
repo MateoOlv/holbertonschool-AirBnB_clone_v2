64615b4504a5fd2a60a5566cda5646aa38ad2be8
MateoOlv <6304@holbertonstudents.com>
2023-08-14 07:18:12 -0700

    5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+imports
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+"""
+Routes
+"""
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return ("Hello HBNB!")
+
+
+"""
+HBNB
+"""
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return ("HBNB")
+
+
+"""
+C is fun
+"""
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    newtext = text.replace("_", " ")
+    return (f"C {newtext}")
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    text = text.replace("_", " ")
+    return (f"Python {text}")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return (f"{n} is a number")
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def numbertemplate(n):
+    return numbertemplate('5-numbert.html', number=n)
+
+
+"""
+Define the host and port that the web app is listening
+"""
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
