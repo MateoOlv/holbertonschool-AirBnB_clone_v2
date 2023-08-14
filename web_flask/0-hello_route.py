190b296767621f333cee0075ab28e285fca4222b
MateoOlv <6304@holbertonstudents.com>
2023-08-14 06:19:19 -0700

    Task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+imports
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+"""
+Routes
+"""
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return ("Hello HBNB!")
+
+
+"""
+Define the host and port that the web app is listening
+"""
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
