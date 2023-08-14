05880d205b3489310d1cfa551141d30ccdb2e2c9
MateoOlv <6304@holbertonstudents.com>
2023-08-14 06:22:57 -0700

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+imports
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+"""
+Routes
+"""
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def index():
+    return ("HBNB")
+
+
+"""
+Define the host and port that the web app is listening
+"""
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
